ba2b1a84e8a9ee85f3cd0e6168ae6845421dfeb6
jay shah <jaypshah99@gmail.com>
2020-11-26 17:45:41 +0530

    added zuul proxy service.

---
+++ employee-information-service-zuul/src/test/java/com/example/employeeinformationservicezuul/EmployeeInformationServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeinformationservicezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeInformationServiceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
